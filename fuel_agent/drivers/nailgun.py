openstack-archive/fuel-web

Added image utils
Added set of image utilities so as to make
it possible to deal with images when they are
available via http or when they are local files,
when they are packed into containers or not, etc.

Change-Id: Id03f6bcaed2a61b4c6d644675ef4cfeb353258a9
Implements: blueprint image-based-provisioning

fuel_agent/fuel_agent/drivers/nailgun.py
@@ -209,11 +209,14 @@ class Nailgun(object):
     def image_scheme(self, partition_scheme):
         data = self.data
         image_scheme = objects.ImageScheme()
+        root_image_uri = 'http://%s/image/%s.img.gz' % (
+            data['ks_meta']['master_ip'],
+            data['profile'].split('_')[0]
+        )
         image_scheme.add_image(
-            uri=data['ks_meta']['image_uri'],
+            uri=root_image_uri,
             target_device=partition_scheme.root_device(),
-            image_format=data['ks_meta']['image_format'],
-            container=data['ks_meta']['image_container'],
-            size=data['ks_meta'].get('image_size'),
+            image_format='ext4',
+            container='gzip',
         )
         return image_scheme

fuel_agent/fuel_agent/utils/img_utils.py
@@ -11,3 +11,80 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import abc
+import requests
+import zlib
+
+
+class Target(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        raise StopIteration()
+
+    def __call__(self, filename='/dev/null'):
+        with open(filename, 'wb') as f:
+            for chunk in self:
+                f.write(chunk)
+            f.flush()
+
+
+class LocalFile(Target):
+    def __init__(self, filename):
+        self.filename = str(filename)
+        self.fileobj = None
+
+    def next(self):
+        if not self.fileobj:
+            self.fileobj = open(self.filename, 'rb')
+        buffer = self.fileobj.read(1048576)
+        if buffer:
+            return buffer
+        else:
+            self.fileobj.close()
+            raise StopIteration()
+
+
+class HttpUrl(Target):
+    def __init__(self, url):
+        self.url = str(url)
+
+    def __iter__(self):
+        return iter(requests.get(self.url, stream=True).iter_content(1048576))
+
+
+class GunzipStream(Target):
+    def __init__(self, stream):
+        self.stream = iter(stream)
+        self.decompressor = zlib.decompressobj(16 + zlib.MAX_WBITS)
+
+    def next(self):
+        try:
+            return self.decompressor.decompress(self.stream.next())
+        except StopIteration:
+            raise
+
+
+class Chain(object):
+    def __init__(self):
+        self.processors = []
+
+    def append(self, processor):
+        self.processors.append(processor)
+
+    def process(self):
+        def jump(proc, next_proc):
+            # if next_proc is just a string we assume it is a filename
+            # and we save stream into a file
+            if isinstance(next_proc, (str, unicode)):
+                proc.target(next_proc)
+                return LocalFile(next_proc)
+            # if next_proc is not a string we return new instance
+            # initialized with the previous one
+            else:
+                return next_proc(proc)
+        return reduce(jump, self.processors)

openstack-archive/fuel-agent

fuel_agent/drivers/nailgun.py
@@ -209,11 +209,14 @@ class Nailgun(object):
     def image_scheme(self, partition_scheme):
         data = self.data
         image_scheme = objects.ImageScheme()
+        root_image_uri = 'http://%s/targetimages/%s.img.gz' % (
+            data['ks_meta']['master_ip'],
+            data['profile'].split('_')[0]
+        )
         image_scheme.add_image(
-            uri=data['ks_meta']['image_uri'],
+            uri=root_image_uri,
             target_device=partition_scheme.root_device(),
-            image_format=data['ks_meta']['image_format'],
-            container=data['ks_meta']['image_container'],
-            size=data['ks_meta'].get('image_size'),
+            image_format='ext4',
+            container='gzip',
         )
         return image_scheme

fuel_agent/utils/img_utils.py
@@ -11,3 +11,81 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import abc
+import requests
+import zlib
+
+
+class Target(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        raise StopIteration()
+
+    def target(self, filename='/dev/null'):
+        with open(filename, 'wb') as f:
+            for chunk in self:
+                f.write(chunk)
+            f.flush()
+
+
+class LocalFile(Target):
+    def __init__(self, filename):
+        self.filename = str(filename)
+        self.fileobj = None
+
+    def next(self):
+        if not self.fileobj:
+            self.fileobj = open(self.filename, 'rb')
+        buffer = self.fileobj.read(1048576)
+        if buffer:
+            return buffer
+        else:
+            self.fileobj.close()
+            raise StopIteration()
+
+
+class HttpUrl(Target):
+    def __init__(self, url):
+        self.url = str(url)
+
+    def __iter__(self):
+        return iter(requests.get(self.url, stream=True).iter_content(1048576))
+
+
+class GunzipStream(Target):
+    def __init__(self, stream):
+        self.stream = iter(stream)
+        #NOTE(agordeev): toggle automatic header detection on
+        self.decompressor = zlib.decompressobj(zlib.MAX_WBITS | 32)
+
+    def next(self):
+        try:
+            return self.decompressor.decompress(self.stream.next())
+        except StopIteration:
+            raise
+
+
+class Chain(object):
+    def __init__(self):
+        self.processors = []
+
+    def append(self, processor):
+        self.processors.append(processor)
+
+    def process(self):
+        def jump(proc, next_proc):
+            # if next_proc is just a string we assume it is a filename
+            # and we save stream into a file
+            if isinstance(next_proc, (str, unicode)):
+                proc.target(next_proc)
+                return LocalFile(next_proc)
+            # if next_proc is not a string we return new instance
+            # initialized with the previous one
+            else:
+                return next_proc(proc)
+        return reduce(jump, self.processors)