openstack-archive/fuel-web

[IBP] move udev rules manipulating out of cloud-init
We need to create udev rules prior the first boot. It's significally
easier to inject files and less magic required.

Change-Id: Iaa4dddad0efe5ac41f6b1284a8c4aa85066e39a9
Closes-Bug: #1420885

fuel_agent/fuel_agent/manager.py
@@ -342,6 +342,26 @@ class Manager(object):
             gu.grub2_cfg(kernel_params=kernel_params, chroot=chroot)
             gu.grub2_install(install_devices, chroot=chroot)
 
+        # FIXME(agordeev) There's no convenient way to perfrom NIC remapping in
+        #  Ubuntu, so injecting files prior the first boot should work
+        with open(chroot + '/etc/udev/rules.d/70-persistent-net.rules',
+                  'wb') as f:
+            f.write('# Generated by fuel-agent during provisioning: BEGIN\n')
+            # pattern is aa:bb:cc:dd:ee:ff_eth0,aa:bb:cc:dd:ee:ff_eth1
+            for mapping in self.configdrive_scheme.common.udevrules.split(','):
+                mac_addr, nic_name = mapping.split('_')
+                f.write('SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", '
+                        'ATTR{address}=="%s", ATTR{type}=="1", KERNEL=="eth*",'
+                        ' NAME="%s"\n' % (mac_addr, nic_name))
+            f.write('# Generated by fuel-agent during provisioning: END\n')
+        # FIXME(agordeev): Disable net-generator that will add new etries to
+        # 70-persistent-net.rules
+        with open(chroot +
+                  '/etc/udev/rules.d/75-persistent-net-generator.rules',
+                  'wb') as f:
+            f.write('# Generated by fuel-agent during provisioning:\n'
+                    '# DO NOT DELETE. It is needed to disable net-generator\n')
+
         with open(chroot + '/etc/fstab', 'wb') as f:
             for fs in self.partition_scheme.fss:
                 # TODO(kozhukalov): Think of improving the logic so as to

openstack-archive/fuel-agent

fuel_agent/manager.py
@@ -342,6 +342,26 @@ class Manager(object):
             gu.grub2_cfg(kernel_params=kernel_params, chroot=chroot)
             gu.grub2_install(install_devices, chroot=chroot)
 
+        # FIXME(agordeev) There's no convenient way to perfrom NIC remapping in
+        #  Ubuntu, so injecting files prior the first boot should work
+        with open(chroot + '/etc/udev/rules.d/70-persistent-net.rules',
+                  'w') as f:
+            f.write('# Generated by fuel-agent during provisioning: BEGIN\n')
+            # pattern is aa:bb:cc:dd:ee:ff_eth0,aa:bb:cc:dd:ee:ff_eth1
+            for mapping in self.configdrive_scheme.common.udevrules.split(','):
+                mac_addr, nic_name = mapping.split('_')
+                f.write('SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", '
+                        'ATTR{address}=="%s", ATTR{type}=="1", KERNEL=="eth*",'
+                        ' NAME="%s"\n' % (mac_addr, nic_name))
+            f.write('# Generated by fuel-agent during provisioning: END\n')
+        # FIXME(agordeev): Disable net-generator that will add new etries to
+        # 70-persistent-net.rules
+        with open(chroot +
+                  '/etc/udev/rules.d/75-persistent-net-generator.rules',
+                  'w') as f:
+            f.write('# Generated by fuel-agent during provisioning:\n'
+                    '# DO NOT DELETE. It is needed to disable net-generator\n')
+
         with open(chroot + '/etc/fstab', 'wb') as f:
             for fs in self.partition_scheme.fss:
                 # TODO(kozhukalov): Think of improving the logic so as to