openstack-archive/fuel-web

IBP: Added driver and objects for building images
As far as building of OS images is nothing more than
just a stage of the whole OS installing procedure
it is sounds rational to implement this in terms
of fuel-agent. Besides, we already have plenty of utilities
which could be useful during building of images.
And some tasks are the same like pre-configuring
some files inside target OS.

Related-bug: #1433193
Implements: blueprint ibp-build-ubuntu-images
Change-Id: I3fadfb16e06e4ee16926da29b7b83ca005500698

fuel_agent/fuel_agent/objects/__init__.py
@@ -18,6 +18,8 @@ from fuel_agent.objects.configdrive import ConfigDrivePuppet
 from fuel_agent.objects.configdrive import ConfigDriveScheme
 from fuel_agent.objects.image import Image
 from fuel_agent.objects.image import ImageScheme
+from fuel_agent.objects.os import OperatingSystem
+from fuel_agent.objects.os import Ubuntu
 from fuel_agent.objects.partition import Fs
 from fuel_agent.objects.partition import Lv
 from fuel_agent.objects.partition import Md
@@ -25,9 +27,13 @@ from fuel_agent.objects.partition import Partition
 from fuel_agent.objects.partition import PartitionScheme
 from fuel_agent.objects.partition import Pv
 from fuel_agent.objects.partition import Vg
+from fuel_agent.objects.repo import DEBRepo
+from fuel_agent.objects.repo import Repo
 
 __all__ = [
     'Partition', 'Pv', 'Vg', 'Lv', 'Md', 'Fs', 'PartitionScheme',
     'ConfigDriveCommon', 'ConfigDrivePuppet', 'ConfigDriveMcollective',
     'ConfigDriveScheme', 'Image', 'ImageScheme',
+    'OperatingSystem', 'Ubuntu',
+    'Repo', 'DEBRepo',
 ]

openstack-archive/fuel-agent

fuel_agent/objects/__init__.py
@@ -16,8 +16,11 @@ from fuel_agent.objects.configdrive import ConfigDriveCommon
 from fuel_agent.objects.configdrive import ConfigDriveMcollective
 from fuel_agent.objects.configdrive import ConfigDrivePuppet
 from fuel_agent.objects.configdrive import ConfigDriveScheme
+from fuel_agent.objects.device import Loop
 from fuel_agent.objects.image import Image
 from fuel_agent.objects.image import ImageScheme
+from fuel_agent.objects.operating_system import OperatingSystem
+from fuel_agent.objects.operating_system import Ubuntu
 from fuel_agent.objects.partition import Fs
 from fuel_agent.objects.partition import Lv
 from fuel_agent.objects.partition import Md
@@ -25,9 +28,14 @@ from fuel_agent.objects.partition import Partition
 from fuel_agent.objects.partition import PartitionScheme
 from fuel_agent.objects.partition import Pv
 from fuel_agent.objects.partition import Vg
+from fuel_agent.objects.repo import DEBRepo
+from fuel_agent.objects.repo import Repo
 
 __all__ = [
     'Partition', 'Pv', 'Vg', 'Lv', 'Md', 'Fs', 'PartitionScheme',
     'ConfigDriveCommon', 'ConfigDrivePuppet', 'ConfigDriveMcollective',
     'ConfigDriveScheme', 'Image', 'ImageScheme',
+    'OperatingSystem', 'Ubuntu',
+    'Repo', 'DEBRepo',
+    'Loop',
 ]