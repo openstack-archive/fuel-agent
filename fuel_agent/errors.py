openstack-archive/fuel-web

[IBP] Add build_utils
Build utils is providing all necessary low-level functions to build
target image.

Related-Bug: #1433193
Partially implements: blueprint ibp-build-ubuntu-images
Change-Id: I8d89377fb38073968a580f7c8e3d04e61ce8ed60

fuel_agent/fuel_agent/errors.py
@@ -146,3 +146,7 @@ class HttpUrlInvalidContentLength(BaseError):
 
 class ImageChecksumMismatchError(BaseError):
     pass
+
+
+class TooManyLoopDevices(BaseError):
+    pass

openstack-archive/fuel-agent

fuel_agent/errors.py
@@ -142,3 +142,7 @@ class HttpUrlInvalidContentLength(BaseError):
 
 class ImageChecksumMismatchError(BaseError):
     pass
+
+
+class NoFreeLoopDevices(BaseError):
+    pass