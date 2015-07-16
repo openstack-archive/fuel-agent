27d35010304bc8eda43edd47c588fb68670eff6b
Sebastian Kalinowski <sebastian@kalinowski.eu>
2015-07-16 15:17:54 +0200

    Split objects/partition.py into smaller files
    Change-Id: I9bc3716b596c426ba4ff3f9348ecf1f0c5116e70
    Implements: blueprint volume-manager-refactoring

--- fuel_agent/objects/__init__.py
+++ fuel_agent/objects/__init__.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from fuel_agent.objects.bootloader import Grub
 from fuel_agent.objects.configdrive import ConfigDriveCommon
 from fuel_agent.objects.configdrive import ConfigDriveMcollective
@@ -22,22 +21,49 @@ from fuel_agent.objects.image import Image
 from fuel_agent.objects.image import ImageScheme
 from fuel_agent.objects.operating_system import OperatingSystem
 from fuel_agent.objects.operating_system import Ubuntu
-from fuel_agent.objects.partition import FS
-from fuel_agent.objects.partition import LV
-from fuel_agent.objects.partition import MD
-from fuel_agent.objects.partition import Parted
-from fuel_agent.objects.partition import Partition
-from fuel_agent.objects.partition import PartitionScheme
-from fuel_agent.objects.partition import PV
-from fuel_agent.objects.partition import VG
+from fuel_agent.objects.partition.fs import FileSystem
+from fuel_agent.objects.partition.lv import LogicalVolume
+from fuel_agent.objects.partition.md import MultipleDevice
+from fuel_agent.objects.partition.parted import Parted
+from fuel_agent.objects.partition.parted import Partition
+from fuel_agent.objects.partition.pv import PhysicalVolume
+from fuel_agent.objects.partition.scheme import PartitionScheme
+from fuel_agent.objects.partition.vg import VolumeGroup
 from fuel_agent.objects.repo import DEBRepo
 from fuel_agent.objects.repo import Repo
 
+
+PV = PhysicalVolume
+VG = VolumeGroup
+LV = LogicalVolume
+MD = MultipleDevice
+FS = FileSystem
+
+
 __all__ = [
-    'Partition', 'Parted', 'PV', 'VG', 'LV', 'MD', 'FS', 'PartitionScheme',
-    'ConfigDriveCommon', 'ConfigDrivePuppet', 'ConfigDriveMcollective',
-    'ConfigDriveScheme', 'Image', 'ImageScheme', 'Grub',
-    'OperatingSystem', 'Ubuntu',
-    'Repo', 'DEBRepo',
+    'Partition',
+    'Parted',
+    'PhysicalVolume',
+    'PV',
+    'VolumeGroup',
+    'VG',
+    'LogicalVolume',
+    'LV',
+    'MultipleDevice',
+    'MD',
+    'FileSystem',
+    'FS',
+    'PartitionScheme',
+    'ConfigDriveCommon',
+    'ConfigDrivePuppet',
+    'ConfigDriveMcollective',
+    'ConfigDriveScheme',
+    'Image',
+    'ImageScheme',
+    'Grub',
+    'OperatingSystem',
+    'Ubuntu',
+    'Repo',
+    'DEBRepo',
     'Loop',
 ]
