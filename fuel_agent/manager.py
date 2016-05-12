2cd6925ce2986b58a3ff1dcade12f55a41bff71e
Ivan Suzdal <isuzdal@mirantis.com>
2016-05-12 16:53:42 +0300

    Remove NVMe devices workaround due to fixed LP #1551432
    This commit was cherry-picked from the commit (master branch):
    f8b132442ad1000dda2802f52a66b241885c407c
    
    Change-Id: I205ed65c19aa01e883d1ca02a136b679ed8eb802
    Closes-Bug: #1580723

--- fuel_agent/manager.py
+++ fuel_agent/manager.py
@@ -1063,8 +1063,7 @@ class Manager(object):
             bu.do_post_inst(chroot,
                             hashed_root_password=root.hashed_password,
                             allow_unsigned_file=CONF.allow_unsigned_file,
-                            force_ipv4_file=CONF.force_ipv4_file,
-                            add_multipath_conf=False)
+                            force_ipv4_file=CONF.force_ipv4_file)
             # restore disabled hosts/resolv files
             bu.restore_resolv_conf(chroot)
             metadata['all_packages'] = bu.get_installed_packages(chroot)
